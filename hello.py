ae0238bcbb353fae960e9551d4028a10a232a115
kimdohoo1102 <kimdohoo1102@gmail.com>
2024-01-12 15:24:08 +0900

    Get and Print User's name
    using input()

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-def say_hello() -> str:
-    return 'heloo'
+def say_hello(uname:str) -> str:
+ return f'Hello, {uname.lower()}'
 
-if __name__ == '__main__' :
-    print(say_hello())
+
+if __name__=='__main__':
+ uinput = input('Enter your name: ')
+ print(say_hello(uinput))
